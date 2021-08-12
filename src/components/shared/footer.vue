20b595520d743da77c0035b95b7dfae99f834435
Jhose Portillo <jhosmar26.00@gmail.com>
2021-08-12 16:43:21 -0500

    EDIT: footer

--- src/components/shared/footer.vue
+++ src/components/shared/footer.vue
@@ -1,121 +1,47 @@
 <template>
-  <footer class="flex flex-col md:flex-row justify-around md:mx-10 mt-12 lg:px-28">
-    <div class="mx-4">
-      <router-link to="/" class="block w-24 md:w-32 mb-8">
-        <img class="w-full h-full" src="../../assets/img/logo.svg" alt="Icon kildstress">
-      </router-link>
-      <ul class="pb-8">
-        <li>
-          <router-link to="/" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Blog
-          </router-link>
-        </li>
-        <li>
-          <a href="/#about-us" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Sobre Nosotros
-          </a>
-        </li>
-        <li>
-          <a href="/#features" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Características
-          </a>
-        </li>
-        <li>
-          <a href="/#contact" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Solicitar una demo
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div class="mx-4">
-      <h3 class="text-xl mb-4">Servicios</h3>
-      <ul class="pb-8">
-        <li>
-          <router-link to="/pentesting" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Pruebas de penetración
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/vulnerability" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Alerta de vulnerabilidades
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/phishing" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Detección de phishing
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/deepweb" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Monitoreo deep web
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/kildstress" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Test de stress
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/academy" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Educación continua
-          </router-link>
-        </li>
-      </ul>
-    </div>
-    <div class="mx-4">
-      <h3 class="text-xl mb-4">Industria</h3>
-      <ul class="pb-8">
-        <li>
-          <router-link to="/financial" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Financiero
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/public-government" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Público/Gobierno
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/educational" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Eduacional
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/ti" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Ciencia, Investigación y TI
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/entertaiment" class="font-inconsalata border-b border-transparent hover:border-bta-blue hover:text-bta-blue">
-            Cultura, Entretenimiento, Deporte
-          </router-link>
-        </li>
-      </ul>
-    </div>
-    <div class="mx-4">
-      <h3 class="text-xl mb-4">Contáctanos</h3>
-      <label class="flex items-center text-white">
-        <feather size="16" type="map-pin" class="pr-2"></feather>
-        <span class="text-sm">Av. Lib. Bdo. O'Higgis #1302 70, Santiago Chile.</span>
-      </label>
-      <label class="flex items-center text-white">
-        <feather size="16" type="smartphone" class="pr-2"></feather>
-        <span class="text-sm">+56 9 4291 0770</span>
-      </label>
-      <label class="flex items-center text-white">
-        <feather size="16" type="mail" class="pr-2 mr-1"></feather>
-        <span class="text-sm">contacto@backtrackacademy.com</span>
-      </label>
-      <div class="flex justify-center md:justify-start py-4">
-        <a href="https://www.facebook.com/groups/BackTrackAcademy" target="_blank" rel="noopener noreferrer" class="hover:text-bta-blue">
-          <feather size="35" type="facebook" class="mr-4"></feather>
-        </a>
-        <a href="https://www.instagram.com/backtrackacademy" target="_blank" rel="noopener noreferrer" class="hover:text-bta-blue">
-          <feather size="35" type="instagram" class="mr-4"></feather>
-        </a>
-        <a href="https://www.linkedin.com/company/backtrack-academy" target="_blank" rel="noopener noreferrer" class="hover:text-bta-blue">
-          <feather size="35" type="linkedin" class="mr-4"></feather>
-        </a>
+  <footer
+    class="flex justify-center items-center bg-total-back px-10 py-24"
+  >
+    <div class="flex flex-row justify-evenly w-full flex-wrap gap-10 sm:gap-4 max-w-screen-2xl">
+      <div class="flex flex-col gap-2 lg:gap-0 text-center sm:text-left">
+        <p class="text-white text-2xl lg:text-5xl font-bold mb-4 lg:mb-8 font-roboto">Nosotros</p>
+        <div class="flex flex-col gap-2 font-ibm">
+          <a class="text-white" href="#">Blog</a>
+          <a class="text-white" href="#">Sobre Nosotros</a>
+          <a class="text-white" href="#">Caracteristicas</a>
+          <a class="text-white" href="#">Solicitar demo</a>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 lg:gap-0 text-center sm:text-left">
+        <p class="text-white text-2xl lg:text-5xl font-bold mb-4 lg:mb-8 font-roboto">Industria</p>
+        <div class="flex flex-col gap-2 font-ibm">
+          <a class="text-white" href="#">Financiero</a>
+          <a class="text-white" href="#">Público/Gobierno</a>
+          <a class="text-white" href="#">Educacional</a>
+          <a class="text-white" href="#">Investigacion</a>
+          <a class="text-white" href="#">Cultura</a>
+          <a class="text-white" href="#">TI</a>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 text-center sm:text-left max-w-xs font-ibm">
+        <p class="text-white text-2xl lg:text-5xl font-bold mb-4 lg:mb-8 font-roboto">Contactos</p>
+        <p class="text-white flex gap-2">
+          <feather class="mt-1" stroke="white" size="18" type="map-pin" /> Av.Lib.O'Higgins
+          #1302 70, Santiago <br />Chile.
+        </p>
+        <p class="text-white flex gap-2">
+          <feather class="mt-1" stroke="white" size="18" type="smartphone" /> +56 9 4291 0770
+        </p>
+        <p class="text-white flex gap-2">
+          <feather class="mt-1" stroke="white" size="18" type="mail" />
+          contacto@backtrackacademy.com
+        </p>
+        <br />
+        <div class="flex gap-4 justify-center sm:justify-start">
+          <feather stroke="white" size="48" type="facebook" />
+          <feather stroke="white" size="48" type="instagram" />
+          <feather stroke="white" size="48" type="linkedin" />
+        </div>
       </div>
     </div>
   </footer>
@@ -124,9 +50,5 @@
 <script>
 export default {
   name: "Footer",
-}
+};
 </script>
-
-<style>
-
-</style>
